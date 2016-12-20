642317fdb3fb7f7cce9091f53b2ad5e73ad580ef
Petar Ivanov <pepincho95@gmail.com>
2016-12-20 21:48:22 +0200

    Add 03_script file.

---
+++ 03_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ls $1/*.txt >fileB
+
+ls $1 | wc -l >>fileB
+
+num_files=`grep $2 $1/*.txt | wc -l`
+
+echo "The number of lines that contains $2 is $num_files"
+
+### Example
+# $ ./03_script.sh ex5 ho
+# The number of lines that contains ho is 11
