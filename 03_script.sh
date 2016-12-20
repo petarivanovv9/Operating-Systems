bd1d283eef1389633ff9ada1137264130023083d
Petar Ivanov <pepincho95@gmail.com>
2016-12-20 21:58:42 +0200

    Update 03_script file.

--- 03_script.sh
+++ 03_script.sh
@@ -8,6 +8,9 @@ num_files=`grep $2 $1/*.txt | wc -l`
 
 echo "The number of lines that contains $2 is $num_files"
 
+echo $@ # all given arguments
+echo $# # the number of all given arguments
+
 ### Example
 # $ ./03_script.sh ex5 ho
 # The number of lines that contains ho is 11
